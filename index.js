738a0fbde5cf3856b714d8bb5f62f8e34c5ee69f
S0YKIM <soyeonkim.9683@gmail.com>
2021-10-08 00:24:28 +0900

    = 반복 계산 및 0 구현 미완성

---
+++ index.js
@@ -0,0 +1,72 @@
+"use strict";
+
+import Calculate from './calculate.js';
+
+const $ = (selector) => document.querySelector(selector);
+
+const calculator = $('.calculator');
+const display = $('.calculator__display');
+const buttons = $('.calculator__buttons');
+const operator = $('.operator')
+
+function App() {
+  const calculate = new Calculate();
+
+  this.init = () => {
+    buttons.addEventListener('click', function (e) {
+      const target = e.target;
+      const info = target.classList[0];
+      const content = target.innerText;
+  
+      if (target.matches('button')) {
+        if (info === ('number' || 'number__zero')) {
+          if (display.innerText.length > 9) return;
+          if (display.innerText === '0') {
+            display.innerText = content;
+            calculate.current = Number(display.innerText);
+          }
+          else if (display.innerText !== '0' && (Number(calculate.previousKey)) || (calculate.previousKey === '.')) {
+            display.innerText += content;
+            calculate.current = Number(display.innerText);
+          }
+          else {
+            display.innerText = content;
+            calculate.current = Number(display.innerText);
+          }
+          calculate.previousKey = content;
+        }
+        
+        if (info === 'operator') {
+            calculate.run();
+            display.innerText = calculate.current;
+            calculate.previous = calculate.current;
+            calculate.previousOperator = content;
+            calculate.previousKey = content;
+        }
+  
+        if (info === 'clear') {
+          calculate.previous = 0;
+          calculate.current = 0;
+          calculate.previousOperator = '';
+          display.innerText = 0;
+        }
+  
+        if (info === 'sign') {
+          calculate.current *= -1;
+          display.innerText = calculate.current;
+        }
+  
+        if (info === 'percentage') {
+          calculate.current /= 100;
+          display.innerText = calculate.current;
+          calculate.previous = calculate.current;
+          calculate.previousOperator = content;
+          calculate.previousKey = content;
+        }
+      }
+    });
+  }
+}
+
+const app = new App();
+app.init();
