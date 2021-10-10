07ef70efe630bb4b3503b4fec937d568c4c3ee9f
S0YKIM <soyeonkim.9683@gmail.com>
2021-10-10 19:25:45 +0900

    Function for each case created | Used switch instead of if | Removed init()

--- index.js
+++ index.js
@@ -12,61 +12,89 @@ const operator = $('.operator')
 function App() {
   const calculate = new Calculate();
 
-  this.init = () => {
-    buttons.addEventListener('click', function (e) {
-      const target = e.target;
-      const info = target.classList[0];
-      const content = target.innerText;
-  
-      if (target.matches('button')) {
-        if (info === 'number' || info === 'number__zero') {
-          if (display.innerText === '0') {
-            display.innerText = content;
-            calculate.current = Number(display.innerText);
-          }
-          else if (display.innerText !== '0' && (Number(calculate.previousKey)) || (calculate.previousKey === '.')) {
-            if (display.innerText.length > 14) return;
-            display.innerText += content;
-            calculate.current = Number(display.innerText);
-          }
-          else {
-            display.innerText = content;
-            calculate.current = Number(display.innerText);
-          }
-          calculate.previousKey = content;
-        }
-        
-        if (info === 'operator') {
-            calculate.run();
-            display.innerText = calculate.current;
-            calculate.previous = calculate.current;
-            calculate.previousOperator = content;
-            calculate.previousKey = content;
-        }
-  
-        if (info === 'clear') {
-          calculate.previous = 0;
-          calculate.current = 0;
-          calculate.previousOperator = '';
-          display.innerText = 0;
-        }
-  
-        if (info === 'sign') {
-          calculate.current *= -1;
-          display.innerText = calculate.current;
-        }
-  
-        if (info === 'percentage') {
-          calculate.current /= 100;
-          display.innerText = calculate.current;
-          calculate.previous = calculate.current;
-          calculate.previousOperator = content;
-          calculate.previousKey = content;
-        }
-      }
-    });
+  const handleNumber = (content) => {
+    if (display.innerText === '0') {
+      display.innerText = content;
+      calculate.current = Number(display.innerText);
+    }
+    else if (display.innerText !== '0' && (Number(calculate.previousKey)) || (calculate.previousKey === '.')) {
+      if (display.innerText.length > 14) return;
+      display.innerText += content;
+      calculate.current = Number(display.innerText);
+    }
+    else {
+      display.innerText = content;
+      calculate.current = Number(display.innerText);
+    }
+    calculate.previousKey = content;
   }
+
+  const handleOperator = (content) => {
+    if (Number(calculate.previousKey) || calculate.previousKey === '.')
+        calculate.run();
+      display.innerText = calculate.current;
+      calculate.previous = calculate.current;
+      calculate.previousOperator = content;
+      calculate.previousKey = content;
+  }
+
+  const handleClear = () => {
+    calculate.previous = 0;
+    calculate.current = 0;
+    calculate.previousOperator = '';
+    calculate.previousKey = '';
+    display.innerText = 0;
+  }
+
+  const handleSign = () => {
+    calculate.current *= -1;
+    display.innerText = calculate.current;
+  }
+
+  const handlePercentage = () => {
+    calculate.current /= 100;
+    display.innerText = calculate.current;
+    calculate.previous = calculate.current;
+    calculate.previousOperator = content;
+    calculate.previousKey = content;
+  }
+
+  buttons.addEventListener('click', function (e) {
+    const target = e.target;
+    const info = target.classList[0];
+    const content = target.innerText;
+
+    if (target.matches('button')) {
+      switch(info) {
+        case 'number':
+          handleNumber(content);
+          break;
+
+        case 'number__zero':
+          handleNumber(content);
+          break;
+        
+        case 'operator':
+          handleOperator(content);
+          break;
+
+        case 'clear':
+          handleClear();
+          break;
+
+        case 'sign':
+          handleSign();
+          break;
+
+        case 'percentage':
+          handlePercentage();
+          break;
+
+        default:
+          return;
+      }
+    }
+  });
 }
 
 const app = new App();
-app.init();
